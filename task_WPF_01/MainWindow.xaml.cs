9b697c350fe5283456d3f4efbfbcb216eb20f438
Валентин Ковальчук <val-81@mail.ru>
2022-01-22 12:06:15 +0300

    Добавьте файлы проекта.

---
+++ task_WPF_01/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task_WPF_01
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class Сalculator : Window
+    {
+        public Сalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Сalculator_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
